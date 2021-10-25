32a0b6accf0381cc8b14f5d873f065d2d92b6ca7
iamveoveo <vinhveoveo21@gmail.com>
2021-10-26 03:37:44 +0700

    Vinh 03:37 10/26/2021

--- search.php
+++ search.php
@@ -2,129 +2,137 @@
 include("template/header.php");
 include("template/header-menu.php");
 ?>
-
-<div class="container">
-    <div class="search mt-4">
-        <input type="text" class="searchTerm" placeholder="Bạn đang tìm kiếm học sinh nào?">
-        <button type="submit" class="searchButton">
-            <i class="fa fa-search"></i>
-        </button>
-    </div>
-    <div class="row m-auto tree-table">
-        <div class="tree col-2">
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">
-                    <a class="btn btn-secondary h-100" data-bs-toggle="collapse" href="#collapse10" role="button" aria-expanded="false" aria-controls="collapse10">Khối 10</a>
-                    <ul class="collapse" id="collapse10">
-                        <li><a href="#">10A1</a></li>
-                        <li><a href="#">10A2</a></li>
-                        <li><a href="#">10D1</a></li>
-                        <li><a href="#">10D2</a></li>
-                    </ul>
-                </li>
-                <li class="list-group-item">
-                    <a class="btn btn-secondary" data-bs-toggle="collapse" href="#collapse11" role="button" aria-expanded="false" aria-controls="collapse11">Khối 11</a>
-                    <ul class="collapse" id="collapse11">
-                        <li><a href="#">11A1</a></li>
-                        <li><a href="#">11A2</a></li>
-                        <li><a href="#">11D1</a></li>
-                        <li><a href="#">11D2</a></li>
-                    </ul>
-                </li>
-                <li class="list-group-item">
-                    <a class="btn btn-secondary" data-bs-toggle="collapse" href="#collapse12" role="button" aria-expanded="false" aria-controls="collapse12">Khối 12</a>
-                    <ul class="collapse" id="collapse12">
-                        <li><a href="#">12A1</a></li>
-                        <li><a href="#">12A2</a></li>
-                        <li><a href="#">12D1</a></li>
-                        <li><a href="#">12D2</a></li>
-                    </ul>
-                </li>
-            </ul>
+<div class="back">
+    <div class="container">
+        <div class="search mb-4 justify-content-center">
+            <div class="w-50 d-flex">
+                <input type="text" class="searchTerm" placeholder="Bạn đang tìm kiếm học sinh nào?">
+                <button type="submit" class="searchButton">
+                    <i class="fa fa-search"></i>
+                </button>
+            </div>
         </div>
-        <div class="my-table col-10">
-            <ul class="list-group">
-                <li>
-                    <a href="#">
-                        <div class="my-table-item row box-shadow">
-                            <div class="item-ava col-3"><img src="" alt=""></div>
-                            <div class="item-detail col-7 flex-fill d-flex flex-column">
-                                <div class="row w-100">
-                                    <div class="col-7"><b>Họ và tên: </b>Đặng Quang Vinh</div>
-                                    <div class="col-5"><b>Ngày sinh: </b>27/5/2001</div>
+        <div class="row m-auto">
+            <div class="tree col-2">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">
+                        <a class="btn btn-secondary h-100" data-bs-toggle="collapse" href="#collapse10" role="button" aria-expanded="false" aria-controls="collapse10">Khối 10</a>
+                        <ul class="collapse" id="collapse10">
+                            <li><a href="#">10A1</a></li>
+                            <li><a href="#">10A2</a></li>
+                            <li><a href="#">10D1</a></li>
+                            <li><a href="#">10D2</a></li>
+                        </ul>
+                    </li>
+                    <li class="list-group-item">
+                        <a class="btn btn-secondary" data-bs-toggle="collapse" href="#collapse11" role="button" aria-expanded="false" aria-controls="collapse11">Khối 11</a>
+                        <ul class="collapse" id="collapse11">
+                            <li><a href="#">11A1</a></li>
+                            <li><a href="#">11A2</a></li>
+                            <li><a href="#">11D1</a></li>
+                            <li><a href="#">11D2</a></li>
+                        </ul>
+                    </li>
+                    <li class="list-group-item">
+                        <a class="btn btn-secondary" data-bs-toggle="collapse" href="#collapse12" role="button" aria-expanded="false" aria-controls="collapse12">Khối 12</a>
+                        <ul class="collapse" id="collapse12">
+                            <li><a href="#">12A1</a></li>
+                            <li><a href="#">12A2</a></li>
+                            <li><a href="#">12D1</a></li>
+                            <li><a href="#">12D2</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul class="list-roll">
+                    <li class="list-roll-item col-5"><a href="#">Học sinh</a></li>
+                    <li class="list-roll-item col-6"><a href="#">Giáo Viên</a></li>
+                    <li class="list-roll-item col-7"><a href="#">Phụ huynh</a></li>
+                </ul>
+            </div>
+            <div class="my-table col-10">
+                <ul class="list-group">
+                    <li>
+                        <a href="#">
+                            <div class="my-table-item row box-shadow">
+                                <div class="item-ava col-3"><img src="" alt=""></div>
+                                <div class="item-detail col-7 flex-fill d-flex flex-column">
+                                    <div class="row w-100">
+                                        <div class="col-7"><b>Họ và tên: </b>Đặng Quang Vinh</div>
+                                        <div class="col-5"><b>Ngày sinh: </b>27/5/2001</div>
+                                    </div>
+                                    <div class="row w-100">
+                                        <div class="col-7"><b>Số điện thoại: </b>0338872927</div>
+                                        <div class="col-5"><b>Giời tính: </b>Nam</div>
+                                    </div>
                                 </div>
-                                <div class="row w-100">
-                                    <div class="col-7"><b>Số điện thoại: </b>0338872927</div>
-                                    <div class="col-5"><b>Giời tính: </b>Nam</div>
-                                </div>
-                            </div>
-                            <div class="item-class col-2 d-flex">
-                                <div>Lớp: 10A1</div>
-                            </div>
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <div class="my-table-item row box-shadow">
-                            <div class="item-ava col-3"><img src="" alt=""></div>
-                            <div class="item-detail col-7 flex-fill d-flex flex-column">
-                                <div class="row w-100">
-                                    <div class="col-7"><b>Họ và tên: </b>Đặng Quang Vinh</div>
-                                    <div class="col-5"><b>Ngày sinh: </b>27/5/2001</div>
-                                </div>
-                                <div class="row w-100">
-                                    <div class="col-7"><b>Số điện thoại: </b>0338872927</div>
-                                    <div class="col-5"><b>Giời tính: </b>Nam</div>
-                                </div>
-                            </div>
-                            <div class="item-class col-2 d-flex">
-                                <div>Lớp: 10A1</div>
-                            </div>
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <div class="my-table-item row box-shadow">
-                            <div class="item-ava col-3"><img src="" alt=""></div>
-                            <div class="item-detail col-7 flex-fill d-flex flex-column">
-                                <div class="row w-100">
-                                    <div class="col-7"><b>Họ và tên: </b>Đặng Quang Vinh</div>
-                                    <div class="col-5"><b>Ngày sinh: </b>27/5/2001</div>
-                                </div>
-                                <div class="row w-100">
-                                    <div class="col-7"><b>Số điện thoại: </b>0338872927</div>
-                                    <div class="col-5"><b>Giời tính: </b>Nam</div>
-                                </div>
-                            </div>
-                            <div class="item-class col-2 d-flex">
-                                <div>Lớp: 10A1</div>
-                            </div>
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <div class="my-table-item row box-shadow">
-                            <div class="item-ava col-3"><img src="" alt=""></div>
-                            <div class="item-detail col-7 flex-fill d-flex flex-column">
-                                <div class="row w-100">
-                                    <div class="col-7"><b>Họ và tên: </b>Đặng Quang Vinh</div>
-                                    <div class="col-5"><b>Ngày sinh: </b>27/5/2001</div>
-                                </div>
-                                <div class="row w-100">
-                                    <div class="col-7"><b>Số điện thoại: </b>0338872927</div>
-                                    <div class="col-5"><b>Giời tính: </b>Nam</div>
+                                <div class="item-class col-2 d-flex">
+                                    <div>Lớp: 10A1</div>
                                 </div>
                             </div>
-                            <div class="item-class col-2 d-flex">
-                                <div>Lớp: 10A1</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <div class="my-table-item row box-shadow">
+                                <div class="item-ava col-3"><img src="" alt=""></div>
+                                <div class="item-detail col-7 flex-fill d-flex flex-column">
+                                    <div class="row w-100">
+                                        <div class="col-7"><b>Họ và tên: </b>Đặng Quang Vinh</div>
+                                        <div class="col-5"><b>Ngày sinh: </b>27/5/2001</div>
+                                    </div>
+                                    <div class="row w-100">
+                                        <div class="col-7"><b>Số điện thoại: </b>0338872927</div>
+                                        <div class="col-5"><b>Giời tính: </b>Nam</div>
+                                    </div>
+                                </div>
+                                <div class="item-class col-2 d-flex">
+                                    <div>Lớp: 10A1</div>
+                                </div>
                             </div>
-                        </div>
-                    </a>
-                </li>
-            </ul>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <div class="my-table-item row box-shadow">
+                                <div class="item-ava col-3"><img src="" alt=""></div>
+                                <div class="item-detail col-7 flex-fill d-flex flex-column">
+                                    <div class="row w-100">
+                                        <div class="col-7"><b>Họ và tên: </b>Đặng Quang Vinh</div>
+                                        <div class="col-5"><b>Ngày sinh: </b>27/5/2001</div>
+                                    </div>
+                                    <div class="row w-100">
+                                        <div class="col-7"><b>Số điện thoại: </b>0338872927</div>
+                                        <div class="col-5"><b>Giời tính: </b>Nam</div>
+                                    </div>
+                                </div>
+                                <div class="item-class col-2 d-flex">
+                                    <div>Lớp: 10A1</div>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <div class="my-table-item row box-shadow">
+                                <div class="item-ava col-3"><img src="" alt=""></div>
+                                <div class="item-detail col-7 flex-fill d-flex flex-column">
+                                    <div class="row w-100">
+                                        <div class="col-7"><b>Họ và tên: </b>Đặng Quang Vinh</div>
+                                        <div class="col-5"><b>Ngày sinh: </b>27/5/2001</div>
+                                    </div>
+                                    <div class="row w-100">
+                                        <div class="col-7"><b>Số điện thoại: </b>0338872927</div>
+                                        <div class="col-5"><b>Giời tính: </b>Nam</div>
+                                    </div>
+                                </div>
+                                <div class="item-class col-2 d-flex">
+                                    <div>Lớp: 10A1</div>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </div>
