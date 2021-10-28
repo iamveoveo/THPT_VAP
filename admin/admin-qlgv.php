bfa4c1a5952a295b12cb886c14c0b598d1b2e8bb
dothingocanh0712 <doanh0712@gmail.com>
2021-10-28 13:51:14 +0700

    Ánh 13:51 10/28/2021

--- admin/admin-qlgv.php
+++ admin/admin-qlgv.php
@@ -21,7 +21,7 @@
                     </div>
                     <div class='card-body'>      
                         <div>
-                            <a class="btn m-3 btn-lg " data-bs-toggle="modal" data-bs-target="#editor" href="#" style="    background-color: #7d9fb9;color: #fff;" role="button">Thêm mới</a>
+                            <a class="btn m-3 btn-lg " data-bs-toggle="modal" data-bs-target="#add" href="#" style="    background-color: #7d9fb9;color: #fff;" role="button">Thêm mới</a>
                         </div>
                         <div class="col-md-12">
                             <div id="table">
@@ -49,7 +49,7 @@
                                             <td>C</td>
                                             <td>D</td>
                                             <td>G</td>
-                                            <td><a href="#"><button type="button" class="btn icon-admin" ><i class="fas fa-edit " ></i></button> </a></td>
+                                            <td><a href="#"><button type="button" class="btn icon-admin" data-bs-toggle="modal" data-bs-target="#add" ><i class="fas fa-edit " ></i></button> </a></td>
                                             <td><a href="#"><button type="button" class="btn btn-danger" ><i class="fas fa-trash-alt "></i></button> </a></td>
                                                 
                                         </tr>
@@ -70,13 +70,102 @@
                             </div>
                     
                         </div>
+
                         <!-- modal add -->
-                        <div class="modal fade" id="editor" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal fade" id="add" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                             <div class="modal-dialog modal-lg">
                                 <div class="modal-content">
                                     <div class="modal-header">
 
                                     <!--form add -->
+                                    <h5 class="modal-title">Thêm thông tin</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+
+                                    <div class="modal-body">
+                                        <div class=" col-12">
+                                            <div class="card h-100" style="background:rgb(88 116 149 / 19%)">
+                                                <div class="card-body">
+                                                    <div class="row gutters">
+                                                        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                                            <h6 class="mb-3 text-primary fs-5 text">Thông tin tài khoản</h6>
+                                                        </div>
+                                                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 mb-3">
+                                                            <div class="form-group">
+                                                                <label for="fullName">Họ và tên</label>
+                                                                <input type="text" class="form-control" name="txtHoTen" >
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 mb-3">
+                                                            <div class="form-group">
+                                                                <label for="eMail">Môn dạy</label>
+                                                                <input type="text" class="form-control" name="txtMon" placeholder="acb@gmail.com">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 mb-3">
+                                                            <div class="form-group">
+                                                                <label for="phone">Số điện thoại</label>
+                                                                <input type="tel" class="form-control " name="sdt" placeholder="09x xxx xxxx">
+                                                            </div>
+                                                        </div>
+                                                        
+                                                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 mb-3">
+                                                            <div class="form-group">
+                                                                <label for="Street">Lớp dạy</label>
+                                                                <input type="text" class="form-control" name="txtLopday" placeholder="VD:10A1,11A2,..">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 mb-3">
+                                                            <div class="form-group">
+                                                                <label for="phone">Email</label>
+                                                                <input type="email" class="form-control " name="txtEmail" placeholder="acb@gmail.com">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 mb-3">
+                                                            <div class="form-group">
+                                                                <label for="phone">Địa chỉ</label>
+                                                                <input type="text" class="form-control " name="txtDiachi" placeholder="Xã,phường/huyện/tỉnh">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 mb-3">
+                                                            <div class="form-group">
+                                                                <label for="website">Ngày sinh</label>
+                                                                <input type="date" class="form-control" name="ngaySinh" >
+                                                            </div>
+                                                        </div>
+
+                                                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 mb-3">
+                                                            <label class="labels">Giới tính</label>
+                                                            <select class="form-select" aria-label="Default select example">
+                                                                <option selected>Chọn giới tính</option>
+                                                                <option value="1">Nam</option>
+                                                                <option value="2">Nữ</option>
+                                                            </select>
+                                                        </div>
+                                                        
+                                                    </div>
+                                                    
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <!-- btn hủy và lưu -->
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" style="background-color: #937da9" data-bs-dismiss="modal">Hủy</button>
+                                        <button type="button" class="btn" name="sbm-import" style="background-color: #3D56B2; color:#fff;" data-bs-dismiss="modal" >Lưu</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                         <!-- modal edit -->
+                         <div class="modal fade" id="editor" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-lg">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+
+                                    <!--form edit -->
                                     <h5 class="modal-title">Sửa thông tin</h5>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                     </div>
