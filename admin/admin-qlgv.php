77109be0bcafe321271594b5bbb78ef91010467f
dothingocanh0712 <doanh0712@gmail.com>
2021-11-03 21:51:15 +0700

    Ánh 2:51 11/3/2021

--- admin/admin-qlgv.php
+++ admin/admin-qlgv.php
@@ -39,7 +39,7 @@
                             <a class="btn m-3 btn-lg " data-bs-toggle="modal" data-bs-target="#add" href="#" style="    background-color: #7d9fb9;color: #fff;" role="button">Thêm mới</a>
                         </div>
                         <div class="col-md-12">
-                            <div id="table_gv" style="max-height:50vh;overflow-y:scroll;">
+                            <div id="table_gv" style="max-height:90vh;overflow-y:scroll;">
                                 <table class="table table-hover table-secondary ">
                                     <thead>
                                         <tr>
@@ -290,7 +290,7 @@
                       
                         <!-- btn import và export -->
                         <!-- btn import và export -->
-                        <div class="center row">
+                        <div class="center row mt-4">
                             <div class="btn-1 col-5">
                                 <a href="#" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
                                 <span>Nhập file</span></a>
